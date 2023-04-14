81a1f867d0a663a425574cc6e9e14e09ba07e151
jbadenho <gawie@wethinkcode.co.za>
2023-04-14 03:13:57 +0200

    sh scripts

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ rm -f $HOME/requirements.sh
 
 wget -P $HOME/ https://raw.githubusercontent.com/wethinkcode/wtc-nix-env/main/src/requirements.sh
 
-./$HOME/requirements.sh
+sh $HOME/requirements.sh
 
 rm $HOME/requirements.sh
 
@@ -15,7 +15,7 @@ rm -f $HOME/nix-folder.sh
 
 wget -P $HOME/ https://raw.githubusercontent.com/wethinkcode/wtc-nix-env/main/src/nix-folder.sh
 
-./$HOME/nix-folder.sh
+sh $HOME/nix-folder.sh
 
 rm $HOME/nix-folder.sh
 
@@ -24,7 +24,7 @@ rm -f $HOME/create-user.sh
 
 wget -P $HOME/ https://raw.githubusercontent.com/wethinkcode/wtc-nix-env/main/src/create-user.sh
 
-./$HOME/create-user.sh
+sh $HOME/create-user.sh
 
 rm $HOME/create-user.sh
 
@@ -33,7 +33,7 @@ rm -f $HOME/nix-install.sh
 
 wget -P $HOME/ https://raw.githubusercontent.com/wethinkcode/wtc-nix-env/main/src/nix-install.sh
 
-./$HOME/nix-install.sh
+sh $HOME/nix-install.sh
 
 rm $HOME/nix-install.sh
 
@@ -42,7 +42,7 @@ rm -f $HOME/hm-install.sh
 
 wget -P $HOME/ https://raw.githubusercontent.com/wethinkcode/wtc-nix-env/main/src/hm-install.sh
 
-./$HOME/hm-install.sh
+sh $HOME/hm-install.sh
 
 rm $HOME/hm-install.sh
 
@@ -51,6 +51,6 @@ rm -f $HOME/nix-env.sh
 
 wget -P $HOME/ https://raw.githubusercontent.com/wethinkcode/wtc-nix-env/main/src/nix-env.sh
 
-./$HOME/nix-env.sh
+sh $HOME/nix-env.sh
 
 rm $HOME/nix-env.sh
