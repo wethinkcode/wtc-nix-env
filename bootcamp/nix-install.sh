0312d6b6bfb748c32f4d2378b7979f4b9e74f9b2
jbadenho <jbadenho@student.wethinkcode.co.za>
2023-04-17 03:35:08 +0200

    clean-up

--- bootcamp/nix-install.sh
+++ bootcamp/nix-install.sh
@@ -2,7 +2,7 @@
 set -e
 
 # installs nix package manager
-sudo runuser -l wtc -c 'sh <(curl -L https://nixos.org/nix/install) --no-daemon'
+sudo runuser -l bootcamp -c 'sh <(curl -L https://nixos.org/nix/install) --no-daemon'
 
-# sources the user wtc terminal
-sudo runuser -l wtc -c '. /home/wtc/.nix-profile/etc/profile.d/nix.sh'
+# sources the user bootcamp terminal
+sudo runuser -l bootcamp -c '. /home/bootcamp/.nix-profile/etc/profile.d/nix.sh'
