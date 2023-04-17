57004ab6d1eee6396548b7b7fc891fbc05665464
jbadenho <jbadenho@student.wethinkcode.co.za>
2023-04-17 09:38:40 +0200

    chmod +x

--- bootcamp/systemd-service.sh
+++ bootcamp/systemd-service.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 set -e
 
+sudo rm -rf /home/bootcamp-profile
+
 sudo mkdir /home/bootcamp-profile
 
 sudo chown root:root /home/bootcamp-profile
 
 sudo rsync -avh --delete /home/bootcamp/ /home/bootcamp-profile/
 
-wget -P /usr/local/bin/ https://raw.githubusercontent.com/wethinkcode/wtc-nix-env/main/bootcamp/bootcamp-update
+wget -P $HOME/tmp_bootcamp/ https://raw.githubusercontent.com/wethinkcode/wtc-nix-env/main/bootcamp/bootcamp-update
+
+sudo mv $HOME/tmp_bootcamp/bootcamp-update /usr/local/bin/bootcamp-update
 
 sudo chown root:root /usr/local/bin/bootcamp-update
 
-wget -P /lib/systemd/system/ https://raw.githubusercontent.com/wethinkcode/wtc-nix-env/main/bootcamp/bootcamp-profile.service
+sudo chmod +x /usr/local/bin/bootcamp-update
 
-wget -P /etc/systemd/ https://raw.githubusercontent.com/wethinkcode/wtc-nix-env/main/bootcamp/bootcamp-profile.service
+wget -P $HOME/tmp_bootcamp/ https://raw.githubusercontent.com/wethinkcode/wtc-nix-env/main/bootcamp/bootcamp-profile.service
+
+sudo mv $HOME/tmp_bootcamp/bootcamp-profile.service /lib/systemd/system/bootcamp-profile.service
+
+sudo chmod a+rwx /lib/systemd/system/bootcamp-profile.service
+
+wget -P $HOME/tmp_bootcamp/ https://raw.githubusercontent.com/wethinkcode/wtc-nix-env/main/bootcamp/bootcamp-profile.service
+
+sudo mv $HOME/tmp_bootcamp/bootcamp-profile.service /etc/systemd/bootcamp-profile.service
 
 sudo chmod a+rwx /etc/systemd/bootcamp-profile.service
 
