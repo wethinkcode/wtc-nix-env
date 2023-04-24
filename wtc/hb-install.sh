7f92c78723d3cd716290987b433a444a918d8c3b
Jan Wick <jbadenho@student.wethinkcode.co.za>
2023-04-24 12:54:57 +0200

    homebrew permissions

--- wtc/hb-install.sh
+++ wtc/hb-install.sh
@@ -3,3 +3,18 @@ set -e
 
 # adds the home-brew channel
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/admin/.profile
+
+sudo groupadd homebrew
+
+sudo chgrp homebrew /home/linuxbrew/
+
+sudo chmod 770 /home/linuxbrew/
+
+sudo chmod +s /home/linuxbrew/
+
+sudo usermod -a -G homebrew wtc
+
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/wtc/.profile
+
