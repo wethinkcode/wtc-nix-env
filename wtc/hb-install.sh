6db12fb5126002e46421b99ae4c6c9e1ea34c016
Jan Wick <jbadenho@student.wethinkcode.co.za>
2023-04-24 13:07:17 +0200

    added homebrew to env

--- wtc/hb-install.sh
+++ wtc/hb-install.sh
@@ -6,15 +6,6 @@ set -e
 
 (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/admin/.profile
 
-sudo groupadd homebrew
+sudo chmod -R a+rwX /home/linuxbrew/
 
-sudo chgrp homebrew /home/linuxbrew/
-
-sudo chmod 770 /home/linuxbrew/
-
-sudo chmod +s /home/linuxbrew/
-
-sudo usermod -a -G homebrew wtc
-
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/wtc/.profile
-
+sudo runuser -l wtc -c "echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/wtc/.profile"
