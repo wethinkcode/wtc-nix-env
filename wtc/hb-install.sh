a91c4ea75097ba429b32f67f16474b641a70dd35
Jan Wick <jbadenho@student.wethinkcode.co.za>
2023-04-24 14:08:54 +0200

    changed command shape

--- wtc/hb-install.sh
+++ wtc/hb-install.sh
@@ -4,7 +4,7 @@ set -e
 # adds the home-brew channel
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-(echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> /home/wtc/.profile
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/admin/.profile
 
 sudo chmod -R a+rwX /home/linuxbrew/
 
