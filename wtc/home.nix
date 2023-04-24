a0ad3a71c75415d80cb38d37c7ad23bbddbdca8c
Jan Wick <jbadenho@student.wethinkcode.co.za>
2023-04-24 13:40:40 +0200

    telegram not working

--- wtc/home.nix
+++ wtc/home.nix
@@ -21,8 +21,7 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    libreoffice
-    slack tdesktop
+    libreoffice slack
     gimp vlc audacity shotcut
     gnupg docker docker-compose 
     git
