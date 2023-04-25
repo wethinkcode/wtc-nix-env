5721c818670d58083ee931628cc08bd6d90dd146
jbadenho <gawie@wethinkcode.co.za>
2023-04-25 04:23:11 +0200

    wrong argument

--- bootcamp_lms/download_lms.sh
+++ bootcamp_lms/download_lms.sh
@@ -7,7 +7,7 @@ read -p "Enter URL of wtc-lms from Slack: " wtc_lms_url
 
 wget -P /home/bootcamp/ $wtc_lms_url
 
-chmod u+x wtc-lms
+#chmod u+x wtc-lms
 
 wget -P /home/bootcamp/ https://raw.githubusercontent.com/wethinkcode/wtc-nix-env/main/bootcamp_lms/path
 
