46ccdf2dd75103f6c0d162a7092799bfbc0d6858
jbadenho <gawie@wethinkcode.co.za>
2023-04-25 09:55:22 +0200

    yaml edit

--- bootcamp_lms/config.py
+++ bootcamp_lms/config.py
@@ -1,16 +1,10 @@
 import yaml
 
 
-def set_state(bootcamp_email):
-    with open("config.yml") as f:
-        doc = yaml.load(f)
-
-    doc["username"] = bootcamp_email
-
-    with open("file_to_edit.yaml", "w") as f:
-        yaml.dump(doc, f)
-
-
 bootcamp_email = input("Please type your email and press Enter: ")
 
-set_state(bootcamp_email)
+with open("config.yml", "r") as file:
+    data = yaml.load(file, Loader=yaml.FullLoader)
+    data["username"] = bootcamp_email
+    with open("config.yml", "w") as file:
+        yaml.dump(data, file)
