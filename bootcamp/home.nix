81f999368342bef34ad6142f2eca57491c755d3f
jbadenho <gawie@wethinkcode.co.za>
2023-04-25 03:16:46 +0200

    minimal bootcamp

--- bootcamp/home.nix
+++ bootcamp/home.nix
@@ -21,11 +21,12 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    slack
-    vlc gimp
-    gnupg git
-    libreoffice
-    neovim vscode
+    git
+    #slack
+    #vlc gimp
+    #gnupg git
+    #libreoffice
+    #neovim vscode
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
