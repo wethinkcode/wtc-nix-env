151e993b9d80b838dac8b60fdf55755baace0cc2
jbadenho <gawie@wethinkcode.co.za>
2023-04-25 03:22:48 +0200

    for editing application menu

--- general/requirements.sh
+++ general/requirements.sh
@@ -2,4 +2,4 @@
 set -e
 
 # required for commands to work
-sudo apt install build-essential net-tools default-jre curl procps file
+sudo apt install build-essential net-tools default-jre curl procps file menulibre
