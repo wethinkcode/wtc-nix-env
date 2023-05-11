cbef29c25905363c71c850ca15ab306d6ee949b8
jbadenho <gawie@wethinkcode.co.za>
2023-05-11 08:18:50 +0200

    creating users

--- bootcamp/create-user.sh
+++ bootcamp/create-user.sh
@@ -2,21 +2,33 @@
 set -e
 
 # delete user
-USR_ID_WTC=$(grep wtc /etc/passwd|cut -d: -f 3)
-USR_ID_CAMPER=$(grep bootcamp /etc/passwd|cut -d: -f 3)
+USR_ID_WTC=$(grep wtc /etc/passwd | cut -d: -f 3)
+USR_ID_CAMPER=$(grep bootcamp /etc/passwd | cut -d: -f 3)
 
-if [[ "${USR_ID_CAMPER}" =~ ^[0-9]+$ ]]; then
+if [[ "${USR_ID_CAMPER}" =~ ^[0-9]+$ ]]
+then
+        sudo userdel -rf bootcamp
+        sudo systemctl disable bootcamp-nix.service
+        sudo systemctl disable bootcamp-profile.service
+        sudo rm -rf /usr/local/bin/bootcamp-nix
+        sudo rm -rf /lib/systemd/system/bootcamp-nix.service
+        sudo rm -rf /etc/systemd/bootcamp-nix.service
+        sudo rm -rf /usr/local/bin/bootcamp-profile
+        sudo rm -rf /lib/systemd/system/bootcamp-profile.service
+        sudo rm -rf /etc/systemd/bootcamp-profile.service
+else
+        echo "No bootcamp user..."
+fi
+
+if [[ "${USR_ID_WTC}" =~ ^[0-9]+$ ]]
+then
         sudo userdel -rf wtc
         sudo systemctl disable wtc-nix.service
         sudo rm -rf /usr/local/bin/wtc-nix
+        sudo rm -rf /lib/systemd/system/wtc-nix.service
+        sudo rm -rf /etc/systemd/wtc-nix.service
 else
         echo "No wtc user..."
-fi
-
-if [[ "${USR_ID_WTC}" =~ ^[0-9]+$ ]]; then
-        sudo userdel -rf bootcamp
-else
-        echo "Creating wtc bootcamp..."
 fi
 
 # creates a standerd user
