3635aae4db7759c27f00241f0177ec41440b4ea2
jbadenho <gawie@wethinkcode.co.za>
2023-05-11 06:27:58 +0200

    updated create ssh file

--- bootcamp-ssh/create-ssh.sh
+++ bootcamp-ssh/create-ssh.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 set -e
 
-passphrase=bootcamper
+
 
 mkdir ~/.ssh 
 
 read -p "Press enter after entering your ssh passphrase (default is bootcamper): "$'\n' -s passphrase
 
-ssh-keygen -t rsa -N $passphrase -C bootcamper -f ~/.ssh/lid_rsa
+if [ "$passphrase" != "" ]
+then
+    ssh-keygen -t rsa -N $passphrase -C bootcamper -f ~/.ssh/lid_rsa
+else
+    default_passphrase=bootcamper
+    ssh-keygen -t rsa -N $default_passphrase -C bootcamper -f ~/.ssh/lid_rsa
+fi
