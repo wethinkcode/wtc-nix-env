2ac1b4ed8ca331dc3ed8d190c3b7e6f7202ce84c
jbadenho <gawie@wethinkcode.co.za>
2023-05-11 06:00:15 +0200

    displaying key fingerprint

--- bootcamp-ssh/create-ssh.sh
+++ bootcamp-ssh/create-ssh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-read -p "Press enter after entering your ssh passphrase: "$'\n' passphrase
+read -p "Press enter after entering your ssh passphrase: "$'\n' -s passphrase
 
-ssh-keygen -t dsa -N $passphrase -C "bootcamper"
+ssh-keygen -t dsa -N $passphrase -C "bootcamper" -f "id_rsa"
--- install-bootcamp-ssh.sh
+++ install-bootcamp-ssh.sh
@@ -7,8 +7,6 @@ rm -rf $HOME/tmp_bootcamp_ssh/
 # create temporary work folder
 mkdir $HOME/tmp_bootcamp_ssh/
 
-
-
 FILE=/home/bootcamp/.ssh/id_rsa.pub
 
 if [ -f "$FILE" ]; then
@@ -35,24 +33,5 @@ else
     cat $FILE
 fi
 
-
-
-# lms config
-rm -f $HOME/tmp_bootcamp_lms/load_config.sh
-
-wget -P $HOME/tmp_bootcamp_lms/ https://raw.githubusercontent.com/wethinkcode/wtc-nix-env/main/bootcamp-lms/load-config.sh
-
-chmod u+x $HOME/tmp_bootcamp_lms/load_config.sh
-
-cd $HOME/tmp_bootcamp_lms/
-
-./load_config.sh
-
-cd $HOME
-
-rm $HOME/tmp_bootcamp_lms/load_config.sh
-
 # clearing temporary work folder
-rm -rf $HOME/tmp_bootcamp_lms/
-
-gnome-session-quit
+rm -rf $HOME/tmp_bootcamp_ssh/
