7fafef45f795d5c29d7dc34f298d06d91b0c8fc4
jbadenho <gawie@wethinkcode.co.za>
2023-05-11 06:23:55 +0200

    ssh key settings

--- bootcamp-ssh/create-ssh.sh
+++ bootcamp-ssh/create-ssh.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
-read -p "Press enter after entering your ssh passphrase: "$'\n' -s passphrase
+passphrase=bootcamper
 
-ssh-keygen -t rsa -N $passphrase -C "bootcamper" -f "id_rsa"
+mkdir ~/.ssh 
+
+read -p "Press enter after entering your ssh passphrase (default is bootcamper): "$'\n' -s passphrase
+
+ssh-keygen -t rsa -N $passphrase -C bootcamper -f ~/.ssh/lid_rsa
